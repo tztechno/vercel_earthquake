16815973cbd37f4e5974c9b641c4e70e251a2157
shun ishii <shun_ishii@shunnoMacBook-puro.local>
2024-09-13 22:43:05 +0900

    2024-09-13

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 echo "============== This is a message from script.sh ==============="
 node fetchEarthquakeData.js
 git add .
-git commit -m "2024-09-04"
+git commit -m "2024-09-13"
 git push -u origin main
 echo "============== New Earthquake Data Added =============="
